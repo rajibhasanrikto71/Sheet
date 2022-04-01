3bada8a965634aff5fff083771ec9d16693a56fc
Villain <83833885+rajibhasanrikto71@users.noreply.github.com>
2022-04-01 22:42:06 +0600

    Add files via upload

---
+++ video-list.js
@@ -0,0 +1,37 @@
+let allVideos = [
+   {
+      name: "Class 1",
+      src: "https://www.youtube.com/embed/Qx4fPOZZmdM",
+      id: "vid_1"
+   },
+   {
+      name: "Class 2",
+      src: "https://www.youtube.com/embed/ezWSFJXzSr4",
+      id: "vid_2"
+   },
+   {
+      name: "Class 3",
+      src: "https://www.youtube.com/embed/0mvZHansGcc",
+      id: "vid_3"
+   },
+   {
+      name: "Class 4",
+      src: "https://www.youtube.com/embed/_gs2JcWwVbo",
+      id: "vid_4"
+   },
+   {
+      name: "Class 5",
+      src: "https://www.youtube.com/embed/KjBqEJ5S9gs",
+      id: "vid_5"
+   },
+   {
+      name: "Class 6",
+      src: "https://www.youtube.com/embed/ETjRmwUin0o",
+      id: "vid_6"
+   },
+   {
+      name: "Class 7",
+      src: "https://www.youtube.com/embed/gbJST6Xl5nA",
+      id: "vid_7"
+   },
+]
